3c90469ce37eb031500833e26f4d6c7b789039bc
Pablo Boing Pscheidt <nikebryt@gmail.com>
2025-01-24 20:12:08 -0300

    Alteração para poder rodar backend com nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,28 +5,19 @@ import express from "express";
 
 import cors from "cors";
 import userRoutes from "./routes/userRoutes";
-import { pool } from "./db";
 
 const app = express();
 
-// Configuração do CORS
 const corsOptions = {
-  origin: "http://localhost:3001",  // Permite o frontend rodando em 3001
+  origin: "http://localhost:3001",
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"], // Adiciona os headers permitidos
+  allowedHeaders: ["Content-Type"],
 };
 
-// Aplica o CORS globalmente antes das rotas
 app.use(cors(corsOptions));
 
-// Configuração de parsing de corpo JSON
-app.use(express.json()); // Para lidar com dados JSON no corpo da requisição
+app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT NOW()");
-  res.send(result.rows);
-});
-
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+app.listen(3000, () => console.log("Running on http://localhost:3000"));
